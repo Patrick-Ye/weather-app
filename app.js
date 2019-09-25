89bfd6f7e9a81269e7a423a612d48659c3938b13
Peng Ye <yep0608@hotmail.com>
2019-09-25 22:31:07 +1000

    25/09

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ const url = 'https://api.darksky.net/forecast/341f559dfb970f33fa1d78e0ba2ee162/3
 request({ url: url, json: true }, (error, response) => {
   console.log(response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degree out')
 })
+
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicGF0cmlja3llIiwiYSI6ImNrMHo4YTV3NzBuam8zbW1paGxqbWZjb2IifQ.2D2PNaT1yiSttwF2KLOhVQ&limit=1'
+
+request({ url: geocodeURL, json: true }, (error, response) => {
+  const latitude = response.body.features[0].center[1]
+  const longitude = response.body.features[0].center[0]
+  console.log(latitude, longitude)
+})
